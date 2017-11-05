c78134d10dba8f571ae969169224e8895bbecf41
degorych <degorych@gmail.com>
2017-11-05 22:28:32 +0200

    Add functions

--- Number words in string/main.c
+++ Number words in string/main.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void fillArray (char* charFillAr, int* intFillAr, int charNum, int intNum, int lens) {
+    int i = 0;
+    for (i = 0; i < lens; i++) {
+        charFillAr[i] = charNum;
+        intFillAr[i] = intNum;
+    }
+}
 
-    /* --- Constants --- */
+void fillIndexArray (char *userArr, int *indexArr, int lens) {
+    int i, l = 0;
+
     const int START_CAPITAL_LETTER_ASCII = 65;
     const int END_CAPITAL_LETTER_ASCII = 90;
     const int START_LOWERCASE_LETTER_ASCII = 97;
     const int END_LOWERCASE_LETTER_ASCII = 122;
-    const int NUMBER_OF_LETTERS_IN_ALPHABET = 52;
+
+    for (i = 0, l = 0; i < lens; i++) {
+        if ((userArr[i] >= START_CAPITAL_LETTER_ASCII && userArr[i] <= END_CAPITAL_LETTER_ASCII)||(userArr[i] >= START_LOWERCASE_LETTER_ASCII && userArr[i] <= END_LOWERCASE_LETTER_ASCII)) {
+            indexArr[l] = i;
+            l++;
+        }
+    }
+    free(userArr);
+}
+
+int numWord(int *indexArr, int lens, int defaultValue) {
+    int numberOfWords = 0;
+    int i = 0;
+
+    for (i = 0; i < lens; i++) {
+        if (indexArr[i] != indexArr[i+1] - 1 && indexArr[i] != defaultValue) {
+            numberOfWords++;
+        }
+    }
+    free(indexArr);
+    return numberOfWords;
+}
+
+int main(){
 
     const int MAX_LENS_OF_STRING = 100000;
+    const int DEFAULT_USER_ARRAY_VALUE = 0;
+    const int DEFAULT_INDEX_ARRAY_VALUE = -1;
 
-    /* --- Counters --- */
-    int i = 0;
-    int k = 0;
-    int l = 0;
-
-    /* --- Allocation of memory for user data --- */
+    /* --- Allocation memory for user data --- */
     char *userString;
     userString = (char*)malloc(MAX_LENS_OF_STRING * sizeof(char));
 
@@ -26,34 +54,16 @@ int main(){
     foundLettersIndex = (int*)malloc(MAX_LENS_OF_STRING * sizeof(int));
 
     /* --- Make all values of userString to "0" and foundLettersIndex to "-1" --- */
-    for (i = 0; i < MAX_LENS_OF_STRING; i++) {
-        userString[i] = 0;
-        foundLettersIndex[i] = -1;
-    }
+    fillArray (userString, foundLettersIndex, DEFAULT_USER_ARRAY_VALUE, DEFAULT_INDEX_ARRAY_VALUE, MAX_LENS_OF_STRING);
 
     printf("Enter string here: ");
     fgets(userString, MAX_LENS_OF_STRING, stdin);
 
-    /* --- Fill foundLettersIndex indexes of letters --- */
-    for (i = 0, l = 0; i < MAX_LENS_OF_STRING; i++) {
-        if ((userString[i] >= START_CAPITAL_LETTER_ASCII && userString[i] <= END_CAPITAL_LETTER_ASCII)||(userString[i] >= START_LOWERCASE_LETTER_ASCII && userString[i] <= END_LOWERCASE_LETTER_ASCII)) {
-            foundLettersIndex[l] = i;
-            l++;
-        }
-    }
-
-    free(userString);
-
-    int numberOfWords = 0;
+    /* --- Fill foundLettersIndex indexes of letters from users array --- */
+    fillIndexArray (userString, foundLettersIndex, MAX_LENS_OF_STRING);
 
     /* --- Count number of words --- */
-    for (i = 0; i < MAX_LENS_OF_STRING; i++) {
-        if (foundLettersIndex[i] != foundLettersIndex[i+1] - 1 && foundLettersIndex[i] != -1) {
-            numberOfWords++;
-        }
-    }
-
-    free(foundLettersIndex);
+    int numberOfWords = numWord(foundLettersIndex, MAX_LENS_OF_STRING, DEFAULT_INDEX_ARRAY_VALUE);
 
     printf("\nNumber of word in string: %d", numberOfWords);
     return 0;
