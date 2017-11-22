b79277c8148bd9b7658fa56a3794b1458cfa5334
degorych <degorych@gmail.com>
2017-11-22 21:49:32 +0200

    Add class Zoo, add Animal class methods, add set and get fun-s

--- Zoo/main.cpp
+++ Zoo/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 const int MAX_ANIMAL_NUMBER = 20;
 
 class Animal {
-public:
+private:
     int age;
     string kind;
     string name;
@@ -21,7 +21,8 @@ public:
     int health;
     bool isShow;
 
-    A () {
+public:
+    Animal() {
         age = 0;
         kind = "0";
         name = "0";
@@ -30,7 +31,8 @@ public:
         isShow = 0;
     }
 
-    void setValue (int valueAge, string valueKind, string valueName, int valuePawsNum, int valueHealth, bool valueIsShow) {
+    /* --- SET --- */
+    void setValue(int valueAge, string valueKind, string valueName, int valuePawsNum, int valueHealth, bool valueIsShow) {
         age = valueAge;
         kind = valueKind;
         name = valueName;
@@ -38,204 +40,346 @@ public:
         health = valueHealth;
         isShow = valueIsShow;
     }
+
+    /* --- SET animal name for search in animal list --- */
+    int setNameForSearch(string nameForSearch) {
+        if (nameForSearch == name) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /* --- Check to name existence --- */
+    int checkToExistenceName() {
+        if (name != "0") {
+            return 1;
+        }
+    }
+
+    /* --- Check to name existence and write him--- */
+    int checkToExistenceName(int counter) {
+        if (name != "0") {
+            cout << counter << ". " << name << endl;
+            return 1;
+        }
+    }
+
+    /* --- GET function for add new animal --- */
+    int getAge() {
+        int valueAge;
+        cout << "Enter age: ";
+        cin >> valueAge;
+        cout << endl;
+        return valueAge;
+    }
+
+    string getKind() {
+        string valueKind;
+        cout << "Enter kind: ";
+        cin >> valueKind;
+        cout << endl;
+        return valueKind;
+    }
+
+    string getName() {
+        string valueName;
+        cout << "Enter name: ";
+        cin >> valueName;
+        cout << endl;
+        return valueName;
+    }
+
+    int getPawsNum() {
+        int valuePawsNum;
+        cout << "Enter paws number: ";
+        cin >> valuePawsNum;
+        cout << endl;
+        return valuePawsNum;
+    }
+
+    int getHealth() {
+        int valueHealth;
+        cout << "Enter health: ";
+        cin >> valueHealth;
+        cout << endl;
+        return valueHealth;
+    }
+
+    bool getIsShow() {
+        bool valueIsShow;
+        cout << "Enter 1, if animal is show, or 0 if animal in zoo: ";
+        cin >> valueIsShow;
+        cout << endl;
+        return valueIsShow;
+    }
+
+    /* --- GET function for edit animal --- */
+    int getAgeEdit() {
+        int valueAge;
+        cout << "Old age: " << age << "; new age: ";
+        cin >> valueAge;
+        cout << endl;
+        return valueAge;
+    }
+
+    string getKindEdit() {
+        string valueKind;
+        cout << "Old kind: " << kind << "; new kind: ";
+        cin >> valueKind;
+        cout << endl;
+        return valueKind;
+    }
+
+    string getNameEdit() {
+        string valueName;
+        cout << "Old name: " << name << "; new name: ";
+        cin >> valueName;
+        cout << endl;
+        return valueName;
+    }
+
+    int getPawsNumEdit() {
+        int valuePawsNum;
+        cout << "Old paws number: " << pawsNum << "; new paws number: ";
+        cin >> valuePawsNum;
+        cout << endl;
+        return valuePawsNum;
+    }
+
+    int getHealthEdit() {
+        int valueHealth;
+        cout << "Old health: " << health << "; new health: ";
+        cin >> valueHealth;
+        cout << endl;
+        return valueHealth;
+    }
+
+    bool getIsShowEdit() {
+        bool valueIsShow;
+        if (isShow != 0) {
+            cout << name << " is show now, select 0 to return in zoo, or 1 to live show: ";
+            cin >> valueIsShow;
+            cout << endl;
+        }
+        else {
+            cout << name << " is not show now, select 1 to send in show, or 0 to live zoo: ";
+            cin >> valueIsShow;
+            cout << endl;
+        }
+        return valueIsShow;
+    }
+
+    /* --- Add new animal --- */
+    void addNewAnimal() {
+        int valueAge, valuePawsNum, valueHealth;
+        string valueKind, valueName;
+        bool valueIsShow;
+
+        valueAge = getAge();
+        valueKind = getKind();
+        valueName = getName();
+        valuePawsNum = getPawsNum();
+        valueHealth = getHealth();
+        valueIsShow = getIsShow();
+
+        setValue(valueAge, valueKind, valueName, valuePawsNum, valueHealth, valueIsShow);
+    }
+
+    /* --- Delete animal --- */
+    void deleteAnimal() {
+        setValue(0, "0", "0", 0, 0, 0);
+    }
+
+    /* --- Show info about animal --- */
+    void showInfoAboutAnimal() {
+        cout << "Age: " << age << endl;
+        cout << "Kind: " << kind << endl;
+        cout << "Paws number: " << pawsNum << endl;
+        cout << "Health " << health << endl;
+        if (isShow != 0) {
+            cout << "Is show" << endl;
+        }
+        else {
+            cout << "Is not show" << endl;
+        }
+    }
+
+    /* --- Edit info about animal --- */
+    void editAnimalInfo() {
+        int editAge, editPawsNum, editHealth;
+        string editKind, editName;
+        bool editIsShow;
+
+        editAge = getAgeEdit();
+        editKind = getKindEdit();
+        editName = getNameEdit();
+        editPawsNum = getPawsNumEdit();
+        editHealth = getHealthEdit();
+        editIsShow = getIsShowEdit();
+
+        setValue(editAge, editKind, editName, editPawsNum, editHealth, editIsShow);;
+    }
 };
 
-class ZooManager {
+class Zoo {
+private:
+    Animal* obj_Animal = new Animal[MAX_ANIMAL_NUMBER];
+    int animalCounter;
+
 public:
-    void animalNotFound (int counter) {
+    Zoo() {
+        animalCounter = 0;
+    }
+
+    /* --- For incorrect animal name or if number of animals is "0" --- */
+    void animalNotFound(int counter) {
         if (counter == MAX_ANIMAL_NUMBER) {
             cout << "Animal not found" << endl;
         }
     }
 
-    void showMenu (Animal* obj) {
+    /* --- Get animal name in user console for search in animal list --- */
+    string getNameAnimalInZoo() {
+        string nameForSearch;
+
+        cin.get();
+        cout << "Enter animal name: ";
+        getline(cin, nameForSearch);
+        cout << endl;
+        return nameForSearch;
+    }
+
+    void addNewAnimal() {
+        int i;
+        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
+            if (obj_Animal[i].checkToExistenceName() != 1) {
+                obj_Animal[i].addNewAnimal();
+                animalCounter++;
+                break;
+            }
+        }
+    }
+
+    void deleteAnimal() {
+        string nameForSearch = getNameAnimalInZoo();
+        int i;
+        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
+            if (obj_Animal[i].setNameForSearch(nameForSearch) == 1) {
+                obj_Animal[i].deleteAnimal();
+                animalCounter--;
+                break;
+            }
+        }
+        animalNotFound(i);
+    }
+
+    void showInfoAboutAnimal() {
+        string nameForSearch = getNameAnimalInZoo();
+        cout << "\n---------- " << nameForSearch << " ----------\n" << endl;
+        int i;
+        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
+            if (obj_Animal[i].setNameForSearch(nameForSearch) == 1) {
+                obj_Animal[i].showInfoAboutAnimal();
+                break;
+            }
+        }
+        animalNotFound(i);
+        cout << "-------------------------\n" << endl;
+    }
+
+    void editAnimalInfo() {
+        string nameForSearch = getNameAnimalInZoo();
+        cout << "\n---------- Edit " << nameForSearch << "----------\n" << endl;
+
+        int i;
+        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
+            if (obj_Animal[i].setNameForSearch(nameForSearch) == 1) {
+                obj_Animal[i].editAnimalInfo();
+                break;
+            }
+        }
+        animalNotFound(i);
+        cout << "----------------------------" << endl;
+    }
+
+    void showAllAnimalInZoo() {
+        int i,j;
+
+        cout << "----- Animal List -----\n" << endl;
+        for (i = 0, j = 1; i < MAX_ANIMAL_NUMBER; i++) {
+            if (obj_Animal[i].checkToExistenceName(j) == 1) {
+                j++;
+            }
+        }
+
+        if (j == 1) {
+            animalNotFound(i);
+        }
+        else {
+            cout << "\nTotal: " << animalCounter << endl;
+        }
+        cout << "\n-----------------------\n" << endl;
+    }
+
+    /* --- Delete animal objects from memory  --- */
+    void deleteAnimals() {
+        delete[] obj_Animal;
+    }
+};
+
+class ZooManager {
+public:
+    /* --- Show menu and user choice --- */
+    int menu() {
         int choiceSelector;
 
-        for (;;) {
-            cout << "  / --- Select action: --- /\n1. Add new animal.\n2. Delete animal.\n3. Show info about animal\n4. Edit animal info.\n5. Show animal list\n6. Exit program" << endl;
-            cout << "\nYou answer: ";
-            cin >> choiceSelector;
-            cout << endl;
+        cout << "  / --- Select action: --- /\n1. Add new animal.\n2. Delete animal.\n3. Show info about animal.\n4. Edit animal info.\n5. Show animal list.\n6. Exit program." << endl;
+        cout << "\nYou answer: ";
+        cin >> choiceSelector;
+        cout << endl;
 
-            switch (choiceSelector) {
+        return choiceSelector;
+    }
+
+    void choiceVariants(Zoo* obj) {
+        for (;;) {
+            int userchoice = menu();
+
+            switch (userchoice) {
                 case ADD_ANIMAL_VALUE:
-                    addNewAnimal(obj);
+                    obj->addNewAnimal();
                     break;
                 case DELETE_ANIMAL_VALUE:
-                    deleteAnimal(obj);
+                    obj->deleteAnimal();
                     break;
                 case SHOW_INFO_ABOUT_ANIMAL:
-                    showInfoAboutAnimal(obj);
+                    obj->showInfoAboutAnimal();
                     break;
                 case EDIT_ANIMAL_INFO_VALUE:
-                    editAnimalInfo(obj);
+                    obj->editAnimalInfo();
                     break;
                 case SHOW_ALL_ANIMAL_IN_ZOO:
-                    showAllAnimalInZoo(obj);
+                    obj->showAllAnimalInZoo();
                     break;
                 case EXIT_PROGRAM:
                     exit(-1);
                 default:
                     cout << "Your answer is incorrect";
-                    exit (-2);
+                    exit(-2);
             }
         }
-    }
-
-    void addNewAnimal (Animal* obj) {
-        int valueAge, valuePawsNum, valueHealth;
-        string valueKind, valueName;
-        bool valueIsShow;
-
-        cout << "Enter age: ";
-        cin >> valueAge;
-        cout << endl;
-
-        cout << "Enter kind: ";
-        cin.get();
-        getline(cin,valueKind) ;
-        cout << endl;
-
-        cout << "Enter name: ";
-        getline(cin, valueName) ;
-        cout << endl;
-
-        cout << "Enter number of paws : ";
-        cin >> valuePawsNum;
-        cout << endl;
-
-        cout << "Enter health: ";
-        cin >> valueHealth;
-        cout << endl;
-
-        cout << "Animal in show? For yes - 1, for no - 0: ";
-        cin >> valueIsShow;
-        cout << endl;
-
-        int i;
-        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
-            if (obj[i].pawsNum == 0) {
-                obj[i].setValue (valueAge, valueKind, valueName, valuePawsNum, valueHealth, valueIsShow);
-                break;
-            }
-        }
-    }
-
-    void deleteAnimal (Animal* obj) {
-        string name;
-        int i;
-
-        cin.get();
-        cout << "Enter animal name: ";
-        getline(cin, name);
-        cout << endl;
-
-        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
-            if (name == obj[i].name) {
-                obj[i].setValue(0, "0", "0", 0, 0, 0);
-                break;
-            }
-        }
-        animalNotFound (i);
-    }
-
-    void showInfoAboutAnimal (Animal* obj) {
-        string name;
-        int i;
-
-        cin.get();
-        cout << "Enter animal name: ";
-        getline(cin, name);
-        cout << "\n---------- " << name << " ----------\n" << endl;
-
-        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
-            if (name == obj[i].name) {
-                cout << name << " age: " << obj[i].age << endl;
-                cout << name << " kind: " << obj[i].kind << endl;
-                cout << name << " paws number: " << obj[i].pawsNum << endl;
-                cout << name << " health " << obj[i].health << endl;
-                if (obj[i].isShow != 0) {
-                    cout << name << " is show" << endl;
-                }
-                else {
-                    cout << name << " is not show" << endl;
-                }
-                break;
-            }
-        }
-        animalNotFound (i);
-        cout << "--------------------" << endl;
-    }
-
-    void editAnimalInfo (Animal* obj) {
-        string name, newKind;
-        int i, newAge, newPawsNum, newHealth;
-        bool newIsShow;
-
-        cin.get();
-        cout << "Enter animal name: ";
-        getline(cin, name);
-        cout << "\n---------- Edit " << name << "----------\n" << endl;
-
-        for (i = 0; i < MAX_ANIMAL_NUMBER; i++) {
-            if (name == obj[i].name) {
-                cout << "Old age: " << obj[i].age << "; new age: ";
-                cin >> newAge;
-                cout << endl;
-
-                cout << "Old kind: " << obj[i].kind << "; new kind: ";
-                cin.get();
-                getline(cin, newKind);
-                cout << endl;
-
-                cout << "Old paws number: " << obj[i].pawsNum << "; new paws number: ";
-                cin >> newPawsNum;
-                cout << endl;
-
-                cout << "Old health: " << obj[i].health << "; new health: ";
-                cin >> newHealth;
-                cout << endl;
-
-                if (obj[i].isShow != 0) {
-                    cout << name << " is show now, select 0 to return in zoo, or 1 to live show: ";
-                    cin >> newIsShow;
-                    cout << endl;
-                }
-                else {
-                    cout << name << " is not show now, select 1 to send in show, or 0 to live zoo: ";
-                    cin >> newIsShow;
-                    cout << endl;
-                }
-                obj[i].setValue (newAge, newKind, name, newPawsNum, newHealth, newIsShow);
-                break;
-            }
-        }
-        animalNotFound (i);
-        cout << "-------------------------" << endl;
-    }
-
-    void showAllAnimalInZoo (Animal* obj) {
-        int i,j;
-        cout << "----- Animal List -----" << endl;
-        for (i = 0, j = 1; i < MAX_ANIMAL_NUMBER; i++) {
-            if (obj[i].pawsNum != 0) {
-                cout << j << ". " << obj[i].name << endl;
-                j++;
-            }
-        }
-        if (j == 1) {
-            animalNotFound (i);
-        }
-        cout << "-----------------------\n" << endl;
     }
 };
 
 int main(){
-    Animal* obj_Animal = new Animal[20];
+    Zoo* obj_Zoo = new Zoo;
     ZooManager* obj_ZooManager = new ZooManager;
-    obj_ZooManager->showMenu(obj_Animal);
+    obj_ZooManager->choiceVariants(obj_Zoo);
 
-    delete []obj_Animal;
+    obj_Zoo->deleteAnimals();
+    delete obj_Zoo;
     delete obj_ZooManager;
 
     return 0;
