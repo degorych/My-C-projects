3d07c95a63643a3458fa08a34c23a2f85cd8c1df
degorych <degorych@gmail.com>
2017-12-03 17:10:05 +0200

    Modify methods and add recursions

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -62,65 +62,71 @@ public:
 
     /* --- Add new node to index --- */
     void addNodeToIndex(int addNumber, int index, Node* newNode) {
-        int checkIndexInShow = checkIndex(index, newNode);
-        int i;
-        Node* currentNode;
-        Node* previousNode;
-        if (checkIndexInShow == 1) {
-            for (i = 0; i <= index; i++) {
-                currentNode = newNode;
-                previousNode = newNode->getPrevious();
-                newNode = newNode->getNext();
-            }
+        checkIndex(index, newNode);
+        if (index == 0) {
+            Node* currentNode = newNode;
+            Node* previousNode = newNode->getPrevious();
             Node* temp = new Node;
+
             temp->setValues(addNumber, previousNode, currentNode);
             currentNode->setPrevious(temp);
+
             if (previousNode != NULL) {
                 previousNode->setNext(temp);
             }
+        }
+        else {
+            index--;
+            newNode = newNode->getNext();
+            addNodeToIndex(addNumber, index, newNode);
         }
     }
 
     /* --- Delete node to index --- */
     void deleteNodeToIndex(int index, Node* delNode) {
-        int checkIndexInShow = checkIndex(index, delNode);
-        int i;
-        Node* previousNode;
-        Node* currentNode;
-        if (checkIndexInShow == 1) {
-            for (i = 0; i <= index; i++) {
-                previousNode = delNode->getPrevious();
-                currentNode = delNode;
-                delNode = delNode->getNext();
-            }
-            previousNode->setNext(delNode);
-            delNode->setPrevious(previousNode);
-            delete currentNode;
+        checkIndex(index, delNode);
+        if (index == 0) {
+            Node* previousNode = delNode->getPrevious();
+            Node* nextNode = delNode->getNext();
+
+            previousNode->setNext(nextNode);
+            nextNode->setPrevious(previousNode);
+            delete delNode;
+        }
+        else {
+            index--;
+            delNode = delNode->getNext();
+            deleteNodeToIndex(index, delNode);
         }
     }
 
-    /* --- Show all nodes value --- */
-    void show (Node* obj) {
-        Node* nodeList = obj;
-        while(nodeList) {
-		cout << nodeList->getNumber() << " ";
-		nodeList = nodeList->getNext();
+    /* --- Show all nodes values --- */
+    void show(Node* obj) {
+        cout << obj->getNumber() << " ";
+
+        if (obj->getNext() != NULL) {
+		obj = obj->getNext();
+		show(obj);
         }
-        cout << endl;
+    }
+
+    /* --- Get node number to user index --- */
+    int getNumberToIndex(int index, Node* obj) {
+        checkIndex(index, obj);
+        if (index == 0) {
+            return obj->getNumber();
+        }
+        else {
+            obj = obj->getNext();
+            index--;
+            getNumberToIndex(index, obj);
+        }
     }
 
     /* --- Show node value to index --- */
-    void showToIndex(int index, Node* obj) {
-        int checkIndexInShow = checkIndex(index, obj);
-        int i;
-        Node* currentNode;
-        if (checkIndexInShow == 1) {
-            for (i = 0; i <= index; i++) {
-                currentNode = obj;
-                obj = obj->getNext();
-            }
-            cout << index << " node is " << currentNode->getNumber() << endl;
-        }
+    void messegeGetNumberToIndex(int index, Node* obj) {
+        int number = getNumberToIndex(index, obj);
+        cout << index << " node is " << number << endl;
     }
 
     /* --- Nodes counter --- */
@@ -135,7 +141,7 @@ public:
     }
 
     /* --- Check index to correct value --- */
-    int checkIndex(int index, Node* obj) {
+    void checkIndex(int index, Node* obj) {
         int nodesNumber = nodeCounter(obj);
         if (index < 0) {
             cout << "Index may be >= 0" << endl;
@@ -145,17 +151,13 @@ public:
             cout << "Index may be less then " << nodesNumber << endl;
             exit (-2);
         }
-        else {
-            return 1;
-        }
     }
 
     /* --- Clean memory --- */
     void cleanMenory(Node* obj) {
-        Node* deleteObjects = obj;
-        while(deleteObjects) {
-		delete deleteObjects->getPrevious();
-		deleteObjects = deleteObjects->getNext();
+        while(obj) {
+		delete obj->getPrevious();
+		obj = obj->getNext();
         }
     }
 };
@@ -172,20 +174,20 @@ int main() {
     objLinList->addNode(50, obj);
     objLinList->addNode(60, obj);
     objLinList->show(obj);
-    cout << "------------------------------------------------------\n" << endl;
+    cout << "\n------------------------------------------------------\n" << endl;
 
     cout << "\n-------------- Add new node to 2 index ---------------" << endl;
     objLinList->addNodeToIndex(55, 2, obj);
     objLinList->show(obj);
-    cout << "------------------------------------------------------\n" << endl;
+    cout << "\n------------------------------------------------------\n" << endl;
 
     cout << "\n--------------- Delete node to 4 index ---------------" << endl;
     objLinList->deleteNodeToIndex(4, obj);
     objLinList->show(obj);
-    cout << "------------------------------------------------------\n" << endl;
+    cout << "\n------------------------------------------------------\n" << endl;
 
     cout << "\n------------ Show node value to 2 index --------------" << endl;
-    objLinList->showToIndex(2, obj);
+    objLinList->messegeGetNumberToIndex(2, obj);
     cout << "------------------------------------------------------\n" << endl;
 
     objLinList->cleanMenory(obj);
